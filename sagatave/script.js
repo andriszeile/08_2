cbbc33a86076f7e0a343ce2523ae3375522a054b
andriszeile <71541542+andriszeile@users.noreply.github.com>
2025-04-30 11:29:42 +0300

    55

--- sagatave/script.js
+++ sagatave/script.js
@@ -3,3 +3,16 @@ function ievade() {
     let v=document.getElementById("nozime").value;
     document.getElementById("izvade").innerHTML="Tu ierakstīji: "+v;
 }
+
+//2.izaicinājums
+function atbilde_ja(){
+    document.getElementById("atb_ir").style.background="green";
+}
+function atbilde_nav(){
+    document.getElementById("atb_nav").style.background="red";
+}
+
+//3.izaicinājums
+function image(){
+    document.getElementById("att").style.display="none";
+}

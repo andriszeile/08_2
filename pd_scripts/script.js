4b61e2383f5688efd6587f3c35f7abfc938459dd
andriszeile <71541542+andriszeile@users.noreply.github.com>
2025-05-14 12:59:25 +0300

    11

---
+++ pd_scripts/script.js
@@ -0,0 +1,20 @@
+// Maina apraksta elementa fonu uz gaiši zilu (4 punkti)
+//Izveidot funkciju, kas nomaina fonu apraksta
+// elementam "info" uz gaiši zilu, ja nospiež
+// uz pogas "Mainīt fonu" 
+
+
+// Parāda lietotāja ievadīto tekstu zem pogas (6 punkti)
+//Izveidot funkciju, kas nospiežot uz pogas 
+// "Parādīt tekstu" nolasa teksta ievades
+// laukā "ievadesLauks" ievadīto tekstu un attēlo
+// nākošajā rindkopā "rezultats" 
+
+
+// Palielina apraksta teksta izmēru (10 punkti)
+//Izveidot funkciju, kas nospiežot uz pogas
+// "Palielināt apraksta tekstu" palielina elementa
+// "skolasApraksts" teksta izmēru uz vērtību 24px
+// P.S. Doto stila atribūtu nāksies sameklēt pašiem!!!
+
+
